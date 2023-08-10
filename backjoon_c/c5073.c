33dd2e2f1616b6a9bb3098a34b3c1b017a4636d4
songwooseung <110340919+songwooseung@users.noreply.github.com>
2023-08-10 21:42:09 +0900

    하

---
+++ backjoon_c/c5073.c
@@ -0,0 +1,20 @@
+//브3
+//백준 5073번
+#include <stdio.h>
+int main()
+{
+    int x,y,z;
+    while(1)
+    {
+        scanf("%d %d %d",&x,&y,&z);
+        if(x == 0 && y == 0 && z == 0) break;
+        if(x==y && y == z && x == z) printf("Equilateral\n");
+        else if(x >= y+z || y >= z + x || z >= x + y)printf("Invalid\n");
+        else if((x==y && x != z) || (x != y && x == z) || y==z) printf("Isosceles\n");
+        else if(x != y && y != z && z != x) printf("Scalene\n");
+    
+    
+         
+       
+    }
+}
