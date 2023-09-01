0c282983a9895b342957c3d381c4362125e3409e
songwooseung <dntmd2061@gmail.com>
2023-09-02 06:50:22 +0900

    체육쌤이 될꺼야

---
+++ backjoon_c/c25314.c
@@ -0,0 +1,14 @@
+// 25314
+// 2023-09-02 06:49:43 (컨트롤 쉬프트 i)
+#include <stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+
+    for(int i = 0; i < n/4; i++)
+    {
+        printf("long ");
+    }
+    printf("int");
+}
