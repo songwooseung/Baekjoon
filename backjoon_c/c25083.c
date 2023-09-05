d2d4dd2560e76c96fca89eeea111395540f59df7
songwooseung <dntmd2061@gmail.com>
2023-09-06 07:29:02 +0900

    새싹

---
+++ backjoon_c/c25083.c
@@ -0,0 +1,14 @@
+// 25083
+// 2023-09-06 07:28:28
+#include <stdio.h>
+
+int main() {
+	printf("         ,r'\"7\n");
+	printf("r`-_   ,'  ,/\n");
+	printf(" \\. \". L_r'\n");
+	printf("   `~\\/\n");
+	printf("      |\n");
+	printf("      |\n");
+
+	return 0;
+}
