a8fa691afcf4319c3080b5484f5a608be11bac49
songwooseung <dntmd2061@gmail.com>
2023-09-09 11:35:01 +0900

    수들의 합

---
+++ backjoon/c1789.c
@@ -0,0 +1,24 @@
+// 1789
+// 2023-09-09 11:34:52
+
+#include <stdio.h>
+int main()
+{
+    long long int input; // 4,294,967,295는 int 범위로 나타낼 수 없기 때문.
+    int S = 0, N = 0;
+
+    scanf("%d",&input);
+
+    while(1)
+    {
+        N++;
+        S += N;
+
+        if(S > input)
+        {
+            N--; // 자연수의 합이 input 값을 넘어가면 N-1을 해주고 반복문을 탈출한다.
+            break;
+        }
+    }
+    printf("%d",N);
+}
