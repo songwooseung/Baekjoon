35a96ba40a4d3c7e5aadac2ee08ce2cd844c1558
songwooseung <dntmd2061@gmail.com>
2023-10-08 12:10:38 +0900

    30204

---
+++ backjoon_py/py30204.py
@@ -0,0 +1,13 @@
+# 30204번: 병영외 급식
+# 2023-10-08 12:10:30
+N, X = map(int,input().split())
+a = list(map(int,input().split()))
+sum = 0
+
+for i in range(N):
+    sum += a[i]
+
+if sum % X == 0:
+    print("1")
+else:
+    print("0")
