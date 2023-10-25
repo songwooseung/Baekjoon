a823f25408659bf4ac6e0c53e1a980ed6d83dc44
songwooseung <dntmd2061@knu.ac.kr>
2023-10-25 22:19:12 +0900

    3040

--- backjoon_py/py3040.py
+++ backjoon_py/py3040.py
@@ -1,4 +1,5 @@
 # 3040
+# 방법 1 
 dwerfs = []
 x = 0
 y = 0
@@ -20,6 +21,19 @@ for i in range(9):
         continue
     else :
         print(dwerfs[i])
-            
-        
-              
+'''
+# 방법 2 sum() 함수 사용
+dwarf = []
+x = 0
+y = 0
+for i in range(9):
+    dwarf.append(int(input()))
+for i in range(8):
+    for j in range(i+1, 9):
+        if sum(dwarf)-dwarf[i]-dwarf[j] == 100:
+            x = i
+            y = j
+for i in range(9):
+    if i != x and i != y:
+        print(dwarf[i])
+''' 
