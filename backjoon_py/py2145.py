31ec93e6f0dff088e134d2c1e0ed94180384818e
songwooseung <dntmd2061@knu.ac.kr>
2023-10-27 19:04:51 +0900

    2145

--- backjoon_py/py2145.py
+++ backjoon_py/py2145.py
@@ -1,4 +1,6 @@
 # 2145
+'''
+# 방법 1
 while(True):
     n = int(input())
     if n == 0:
@@ -10,5 +12,13 @@ while(True):
             tmp += n % 10
             n //= 10
         n = tmp # tmp의 값인 16을 n에 다시 넣어주고 n >= 10인지 확인해주고 다시 while(n>0) 반복문 돌아감
+    print(n)    
+'''
+# 방법 2 (sum 함수와 map함수를 이용한 문제 풀이)
+while (True):
+    n = int(input())
+    if n == 0 :
+        break
+    while(n >= 10):
+        n = sum(map(int,str(n))) # n을 문자열로 바꾸고 map함수를 이용해 각 자리수를 int형으로 바꾸고 sum함수를 이용해 더해줌
     print(n)
-            
