e4fdcb41da2835a54c9c9ea7e156152fdb20b45b
songwooseung <dntmd2061@knu.ac.kr>
2023-11-09 22:00:01 +0900

    10768

---
+++ backjoon_py/py10768.py
@@ -0,0 +1,11 @@
+m = int(input())
+d = int(input())
+
+if m == 2 and d == 18 :
+    print("Special")
+elif m == 1 :
+    print("Before")
+elif m == 2 and d < 18 :
+    print("Before")
+else :
+    print("After")
