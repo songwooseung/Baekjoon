b8eb5568376164902ff87b280953bc1467feb046
송우승 <110340919+songwooseung@users.noreply.github.com>
2024-04-04 07:48:43 +0000

    3986 stack

--- backjoon_py/py3986.py
+++ backjoon_py/py3986.py
@@ -2,18 +2,16 @@ import sys
 input = sys.stdin.readline
 
 N = int(input())
-cnt = 0 
+cnt = 0
 
 for i in range(N):
     stack = []
-    arr = list(input().strip())
-    for j in arr :
-        if len(stack) == 0:
-            stack.append(j)
-        elif stack[-1] == j :
+    word = input().strip()
+    for j in word :
+        if stack and stack[-1] == j :
             stack.pop(-1)
-        else : 
+        else :
             stack.append(j)
-    if len(stack) == 0:
-        cnt+=1
+    if not stack :
+        cnt += 1
 print(cnt)
