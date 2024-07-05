8517e0716b09e5d057d8a7360d8c90eda7e8b857
송우승 <110340919+songwooseung@users.noreply.github.com>
2024-07-05 04:40:25 +0000

    10819 backtracking and broteforce

--- backjoon_py/py10819.py
+++ backjoon_py/py10819.py
@@ -1,4 +1,4 @@
-# 정수의 모든 경우의 수를 순서대로 뽑아주는 함수
+# 1. itertools 함수로 푼 방법
 # from itertools import permutations
 import sys 
 from itertools import permutations 
@@ -19,3 +19,29 @@ for li in arr2 :
     result = max(result, total)
 
 print(result)
+
+# 2. 백트래킹으로 구현한 방법 
+
+# n = int(input())
+# in_list = list(map(int ,input().split()))
+# visited = [False]*n
+# answer = 0
+# def sol(li):
+#   global answer
+#   if len(li) == n:
+#     total = 0
+#     for i in range(n-1):
+#       total += abs(li[i]- li[i+1])
+#     answer = max(answer, total)
+#     return
+
+#   for i in range(n):
+#     if not visited[i]:
+#       visited[i] = True
+#       li.append(in_list[i])
+#       sol(li)
+#       visited[i] = False
+#       li.pop()
+
+# sol([])
+# print(answer)
