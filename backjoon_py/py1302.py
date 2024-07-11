f7b884f09fdebc483c4f67936a282b3a40c01e1d
송우승 <110340919+songwooseung@users.noreply.github.com>
2024-07-11 01:55:27 +0000

    1302 dict sort

--- backjoon_py/py1302.py
+++ backjoon_py/py1302.py
@@ -13,9 +13,10 @@ for i in range(N):
     else : 
         arr[book] += 1 
 
-
+# lambda 함수는 간단한 연산이나 일회성 작업에 유용
 # 내림차순 : x를 -로 바꿔서 정렬을 역으로 바꿈.
-# 1차 기준은 값을 기준으로 2차 기준은 책 제목을 기준으로  
+# lambda에서 key는 정렬의 기준을 나타낸다.
+# 1차 기준은 값을 기준으로 2차 기준은 책 제목을 기준으로 
 arr2 = sorted(arr.items(),key=lambda x : (-x[1],x[0]))
 
 print(arr2[0][0])
