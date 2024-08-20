b175bb71698794c93fdcd2904836a4b2f0db91cf
송우승 <110340919+songwooseung@users.noreply.github.com>
2024-08-20 06:06:16 +0000

    1874 stack

--- backjoon_py/py1874.py
+++ backjoon_py/py1874.py
@@ -1,5 +1,7 @@
-N = int(input())
-tf = True 
+# sys와 input의 실행 속도 차이가 매우 심함을 알 수 있다.
+import sys
+
+N = int(sys.stdin.readline())
 cnt = 1
 
 pp = [] 
@@ -7,7 +9,7 @@ stack = []
 
 # 올바른 수열은 num과 -의 길이가 무조건 동일하게 나옴.
 for _ in range(N):
-    num = int(input())
+    num = int(sys.stdin.readline())
     
     # num 이 cnt보다 크거나 같을 때만 실행
     while num >= cnt : 
@@ -19,10 +21,7 @@ for _ in range(N):
         stack.pop()
         pp.append('-')
     
-    else :
-        tf = False
-
-if tf :
+if not stack :
     for i in pp :
         print(i)
 else :
